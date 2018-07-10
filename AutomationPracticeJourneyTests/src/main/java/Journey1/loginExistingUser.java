5985a76e6e225e7b9af5b5a6627151cfb0ecc452
matthew.mccormick <matthew_mccormick@hotmail.co.uk>
2018-07-10 13:14:01 +0100

    Created a DataProviders package in Utilities. Moved accounts from test into DataProviders

--- AutomationPracticeJourneyTests/src/main/java/Journey1/loginExistingUser.java
+++ AutomationPracticeJourneyTests/src/main/java/Journey1/loginExistingUser.java
@@ -7,19 +7,15 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import HomePage.*;
 import ru.yandex.qatools.allure.annotations.TestCaseId;
+import DataProviders.accounts;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assert_;
 
 public class loginExistingUser extends BaseUITest
 {
-    @DataProvider(name = "Existing Users")
-    public static Object[][] existingUsers()
-    {
-        return new Object[][]{{"Testing123@testmail.com", "s3cur3P4ssw0rd"},{"Testing321@testmail.com","v3rys3cur3P4ssw0rd@"}};
-    }
     @TestCaseId("RegNew1")
-    @Test(description = "Registering a new user at Automation Practice", dataProvider = "Existing Users")
+    @Test(description = "Registering a new user at Automation Practice", dataProvider = "existingUsers", dataProviderClass = DataProviders.accounts.class)
     public void loginAsExistingUser(String email, String password)
     {
         LandingPage landingPage = LandingPage
---
+++ utilities/src/main/java/DataProviders/accounts.java
@@ -0,0 +1,12 @@
+package DataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class accounts {
+
+    @DataProvider(name = "Existing Users")
+    public static final Object[][] existingUsers()
+    {
+        return new Object[][]{{"Testing123@testmail.com", "s3cur3P4ssw0rd"},{"Testing321@testmail.com","v3rys3cur3P4ssw0rd@"}};
+    }
+}
